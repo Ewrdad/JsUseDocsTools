bd05f767bc07778c673e1e8ec6188f8243b73320
Ewrdad <ewrdad12@gmail.com>
2024-07-14 22:54:40 +0100

    Add resizable panels to FileFinder component

--- JsUseDocTools/src/App/ProjectViewer/ProjectOverview/FileFinder/FileFinder.jsx
+++ JsUseDocTools/src/App/ProjectViewer/ProjectOverview/FileFinder/FileFinder.jsx
@@ -1,5 +1,10 @@
 import { Paper } from "@mui/material/index";
 import { ScrollArea } from "../../../../shadcn/components/ui/scroll-area";
+import {
+  ResizablePanel,
+  ResizablePanelGroup,
+  ResizableHandle,
+} from "@/shadcn/components/ui/resizable";
 
 /**
  * MARK: File Finder
@@ -8,28 +13,46 @@ import { ScrollArea } from "../../../../shadcn/components/ui/scroll-area";
  */
 export const FileFinder = () => {
   return (
-    <Paper>
-      <h1>From Package.json</h1>
-      <ScrollArea className="h-[200px] w-full rounded-md border p-4">
-        <p> Main: fdfd/dsads</p>
-        <p>directories.lib : asdasd</p>
-        <p>directories.bin : asdasd</p>
-        <p>directories.man : asdasd</p>
-        <p>directories.test : asdasd</p>
-        <p>directories.example : asdasd</p>
-        <p>directories.doc : asdasd</p>
-      </ScrollArea>
+    <Paper className="w-full">
+      <ResizablePanelGroup direction="vertical" className="min-h-96">
+        <h1 className="p-2 font-bold">From Package.json</h1>
+        <ResizablePanel defaultSize={3}>
+          <ScrollArea className="w-full h-full p-4 border rounded-md">
+            <p> Main: fdfd/dsads</p>
+            <p>directories.lib : asdasd</p>
+            <p>directories.bin : asdasd</p>
+            <p>directories.man : asdasd</p>
+            <p>directories.test : asdasd</p>
+            <p>directories.example : asdasd</p>
+            <p>directories.doc : asdasd</p>
+          </ScrollArea>
+        </ResizablePanel>
+        <ResizableHandle withHandle />
+        <h1 className="p-2 font-bold">From root</h1>
+        <ResizablePanel defaultSize={3}>
+          <ScrollArea className="w-full h-full p-4 border rounded-md">
+            <p>./src</p>
+            <p>./lib</p>
+            <p>./bin</p>
+            <p>package.json </p>
+            <p>README.md </p>
+            <p>index.js </p>
+          </ScrollArea>
+        </ResizablePanel>
+        <ResizableHandle withHandle />
+        <h1 className="p-2 font-bold">From Misc</h1>
 
-      <br />
-      <h1>From root</h1>
-      <ScrollArea className="h-[200px] w-full rounded-md border p-4">
-        <p>./src</p>
-        <p>./lib</p>
-        <p>./bin</p>
-        <p>package.json </p>
-        <p>README.md </p>
-        <p>index.js </p>
-      </ScrollArea>
+        <ResizablePanel defaultSize={3}>
+          <ScrollArea className="w-full h-full p-4 border rounded-md">
+            <p>./src</p>
+            <p>./lib</p>
+            <p>./bin</p>
+            <p>package.json </p>
+            <p>README.md </p>
+            <p>index.js </p>
+          </ScrollArea>
+        </ResizablePanel>
+      </ResizablePanelGroup>
     </Paper>
   );
 };
